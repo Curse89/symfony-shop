04d30d990077ca513fa41fa300e87fad2ea49ff4
Сергей <curse1010@gmail.com>
2021-09-25 23:38:36 +0300

    Дефолт контроллер: добавление товара в БД, вывод списка товаров через ДД

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\{Response, Request};
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
@@ -13,8 +14,27 @@ class DefaultController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('main/default/index.html.twig', [
-            'controller_name' => 'DefaultController',
-        ]);
+        $entityManager = $this->getDoctrine()->getManager();
+        $productList = $entityManager->getRepository(Product::class)->findAll();
+        dd($productList);
+        return $this->render('main/default/index.html.twig', []);
+    }
+
+    /**
+     * @Route("/product-add", name="product_add")
+     */
+    public function productAdd(Request $request): Response
+    {
+        $product = new Product();
+        $product->setTitle('Product' . rand(1, 100));
+        $product->setDescription('smth');
+        $product->setPrice(10);
+        $product->setQuantity(1);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($product);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('homepage');
     }
 }
